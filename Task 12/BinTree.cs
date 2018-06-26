5686baee8ffcc3aa1533b031497bf9b4b39aa03d
novojilovakatya <ekaterina_in2011@mail.ru>
2018-06-26 11:00:05 +0500

    Task 12

---
+++ Task 12/BinTree.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_12
+{
+    class BinTree
+    {
+        public int data;
+        public BinTree left,  //адрес левого поддерева 
+                       right; //адрес правого поддерева
+        public BinTree()
+        {
+            data = 0;
+            left = null;
+            right = null;
+        }
+        public BinTree(int d)
+        {
+            data = d;
+            left = null;
+            right = null;
+        }
+        public override string ToString()
+        {
+            return data + " ";
+        }
+
+        public static int[] FindTree(int[] arr, out int compare, out int forw)
+        {
+            compare = 0;
+            forw = 0;
+            BinTree tree = new BinTree(arr[0]);
+            for (int i = 1; i < arr.Length; i++)
+            {
+                BinTree p = tree;//корень дерева
+                BinTree r = null;
+
+
+                while (p != null)
+                {
+                    r = p;
+                    compare += 2;
+                    if (arr[i] < p.data) p = p.left;        //пойти в левое поддерево
+                    else p = p.right;                       //пойти в правое поддерево
+                }
+
+                compare += 2;
+                // если d<r->key, то добавляем его в левое поддерево
+                if (arr[i] < r.data) r.left = new BinTree(arr[i]);
+
+                // если d>r->key, то добавляем его в правое поддерево
+                else r.right = new BinTree(arr[i]);
+            }
+
+            int index = 0;
+            ShowTree(tree, ref arr, ref index, ref compare, ref forw);
+            return arr;
+        }
+
+        public static void ShowTree(BinTree p, ref int[] mas, ref int index, ref int compare, ref int forw)
+        {
+            if (p != null)
+            {
+                BinTree.ShowTree(p.left, ref mas, ref index, ref compare, ref forw);//переход к левому поддереву
+                forw++;
+                mas[index] = p.data;
+                index++;
+                BinTree.ShowTree(p.right, ref mas, ref index, ref compare, ref forw);//переход к правому поддереву
+            }
+        }
+
+    }
+}
---
+++ Task 12/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_12
+{
+    class Program
+    {
+        static void BubbleSort(int[] arr)
+        {
+            int compare = 0;
+            int forw = 0;
+            for (int i = 0; i < arr.Length; i++)
+                for (int j = 0; j < arr.Length - i - 1; j++)
+                {
+                    compare++;
+                    if (arr[j] > arr[j + 1])
+                    {
+                        forw ++;
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            Console.WriteLine("Сравнений в сортировке пузырьком: {0}", compare);
+            Console.WriteLine("Пересылок в сортировке пузырьком: {0}", forw);
+        }
+
+        static void BinTreeSort(int[] arr)
+        {
+            int compare = 0, forw = 0;
+            arr = BinTree.FindTree(arr, out compare, out forw);
+            Console.WriteLine("Сравнений в сортировке с помощью двоичного дерева: {0}", compare);
+            Console.WriteLine("Пересылок в сортировке с помощью двоичного дерева: {0}", forw);
+
+        }
+
+        static void Main(string[] args)
+        {
+            Random a = new Random();
+            int[] masUp1 = new int[1000];
+            int[] masUp2 = new int[1000];
+            int[] masDown1 = new int[1000];
+            int[] masDown2 = new int[1000];
+            int[] masChaos1 = new int[1000];
+            int[] masChaos2 = new int[1000];
+
+            for (int i = 0; i < 1000; i++)
+            {
+                masUp1[i] = i;
+                masUp2[i] = i;
+                masDown1[i] = 1000 - i;
+                masDown2[i] = 1000 - i;
+                masChaos1[i] = a.Next(1000);
+                masChaos2[i] = masChaos1[i];
+            }
+
+            Console.WriteLine("Сортировка массива упорядоченного по возрастанию");
+            BubbleSort(masUp1);
+            BinTreeSort(masUp2);
+
+            Console.WriteLine();
+            Console.WriteLine("Сортировка массива упорядоченного по убыванию");
+            BubbleSort(masDown1);
+            BinTreeSort(masDown2);
+
+            Console.WriteLine();
+            Console.WriteLine("Сортировка неупорядоченного массива");
+            BubbleSort(masChaos1);
+            BinTreeSort(masChaos2);
+            Console.ReadLine();
+        }
+    }
+}
